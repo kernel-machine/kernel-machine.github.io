09cd918f08c665a06703bb48a47aec09dd376a12
Luca Giovannesi <l.giovannesi@gmail.com>
2026-09-23 20:55:46 +0000

    Remove address filter from contact links and adjust fetch caching strategy

--- src/vue/stack/DataManager.vue
+++ src/vue/stack/DataManager.vue
@@ -106,7 +106,7 @@ const _loadSectionJsonFiles = async () => {
 
 const _loadJson = async (path) => {
     try {
-        const response = await fetch(basePath + "/data/" + path, { cache: "no-store" })
+        const response = await fetch(basePath + "/data/" + path)
         return await response.json()
     }
     catch (e) {
